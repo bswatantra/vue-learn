3a68405bbdb6365fa7ba7430e8851a1859c6ac2f
swatantra <swatantra440ry@gmail.com>
2021-07-02 15:41:36 +0545

    read me updata

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,50 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
-    public function index(): JsonResponse
-    {
-        $posts = Post::latest()->paginate(1);
-        return response()->json($posts);
-    }
+	public function index(): JsonResponse
+	{
+		$posts = Post::latest()->paginate(1);
+		return response()->json($posts);
+	}
 
-    public function store(): JsonResponse
-    {
-        request()->validate([
-            'title' => 'required|min:10|max:255',
-            'content' => 'required',
-        ]);
+	public function store(): JsonResponse
+	{
+		request()->validate([
+			'title' => 'required|min:10|max:255',
+			'content' => 'required',
+		]);
 
-        $post = Post::create(request()->only('title', 'content'));
-        return response()->json($post);
-    }
-
-
-    public function show(Post $post): JsonResponse
-    {
-        return response()->json($post);
-    }
+		$post = Post::create(request()->only('title', 'content'));
+		return response()->json($post);
+	}
 
 
-    public function update(Request $request, Post $post): JsonResponse
-    {
-        request()->validate([
-            'title' => 'required|min:10|max:255',
-            'content' => 'required',
-        ]);
-
-        $post = Post::update(request()->only('title', 'content'));
-        return response()->json($post);
-    }
+	public function show(Post $post): JsonResponse
+	{
+		return response()->json($post);
+	}
 
 
-    public function destroy($post): JsonResponse
-    {
-        Post::destroy($post);
-        return response()->json("ok");
-    }
+	public function update(Request $request, Post $post): JsonResponse
+	{
+		request()->validate([
+			'title' => 'required|min:10|max:255',
+			'content' => 'required',
+		]);
+
+		$post = Post::update(request()->only('title', 'content'));
+		return response()->json($post);
+	}
+
+
+	public function destroy($post): JsonResponse
+	{
+		Post::destroy($post);
+		return response()->json("ok");
+	}
 }
